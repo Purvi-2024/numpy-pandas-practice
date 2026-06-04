1804a2ab3c19880a78e2ee740f6c1f674612f30c
Purvi-2024 <purvibamba@gmail.com>
2026-06-04 18:40:50 +0530

    Completed NumPy Day 3 Array Operations

---
+++ day3_operations.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Original Array:", arr)
+
+print("Add 10:", arr + 10)
+
+print("Subtract 5:", arr - 5)
+
+print("Multiply by 2:", arr * 2)
+
+print("Divide by 2:", arr / 2)
